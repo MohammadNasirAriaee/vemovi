33d8867624e7ab253311e487c014760ff80e3163
Mohammad Nasir Ariaee <NasirAriaee@gmail.com>
2024-06-23 10:05:49 +0430

    first commit

---
+++ src/pages/MovieList.jsx
@@ -0,0 +1,17 @@
+import { Card } from "../components/Card";
+import { useFetch } from "../hooks/useFetch";
+
+export const MovieList = ({ apiPath }) => {
+  const { data: movies } = useFetch(apiPath);
+
+  return (
+    <main>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap other:justify-evenly">
+          {movies &&
+            movies.map((movie) => <Card key={movie.id} movie={movie} />)}
+        </div>
+      </section>
+    </main>
+  );
+};
