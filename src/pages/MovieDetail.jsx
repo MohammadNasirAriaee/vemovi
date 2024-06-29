7c7446b12221fd1ddbac771f68de3e26a22c6374
Mohammad Nasir Ariaee <NasirAriaee@gmail.com>
2024-06-29 15:13:20 +0430

    removed unnecessary codes from moviedetail

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
 import BackUp from "../assets/images/backup.png";
